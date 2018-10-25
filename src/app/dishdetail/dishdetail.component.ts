ad1b35969ec8f0c423c8e5dc9e18ea0fc62ddbe6
esterkane <sruzic@googlemail.com>
2018-10-25 12:42:21 +0200

    Data Binding

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Dish } from '../shared/dish';
 
 const DISH = {
   id: '0',
@@ -51,8 +52,9 @@ const DISH = {
 })
 export class DishdetailComponent implements OnInit {
 
-  dish = DISH;
-  
+  @Input()
+  dish: Dish;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+
 export class Dish {
     id: string;
     name: string;
@@ -7,4 +9,6 @@ export class Dish {
     label: string;
     price: string;
     description: string;
+
+    comments: Comment[];
 }
