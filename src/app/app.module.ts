effe1a7044014a1c5cd475a4ec21c765d0561c72
esterkane <sruzic@googlemail.com>
2018-10-30 11:52:56 +0100

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +33,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DishService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
