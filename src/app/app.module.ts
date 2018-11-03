0e97e03d5d3a16312d53a168d9e11643dd2fdfbf
esterkane <sruzic@googlemail.com>
2018-11-03 15:52:53 +0100

    Assignment 3: Angular Forms and Reactive Programming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule }          from '@angular/material/slider';
 
 
 @NgModule({
@@ -61,6 +62,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatButtonModule,
     MatDialogModule,
     MatProgressSpinnerModule,
+    MatSliderModule,
     FormsModule,
     ReactiveFormsModule
   ],
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,55 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
-
-const DISH = {
-  id: '0',
-  name: 'Uthappizza',
-  image: '/assets/images/uthappizza.png',
-  category: 'mains',
-  featured: true,
-  label: 'Hot',
-  price: '4.99',
-  // tslint:disable-next-line:max-line-length
-  description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-  comments: [
-       {
-           rating: 5,
-           comment: 'Imagine all the eatables, living in conFusion!',
-           author: 'John Lemon',
-           date: '2012-10-16T17:57:28.556094Z'
-       },
-       {
-           rating: 4,
-           comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-           author: 'Paul McVites',
-           date: '2014-09-05T17:57:28.556094Z'
-       },
-       {
-           rating: 3,
-           comment: 'Eat it, just eat it!',
-           author: 'Michael Jaikishan',
-           date: '2015-02-13T17:57:28.556094Z'
-       },
-       {
-           rating: 4,
-           comment: 'Ultimate, Reaching for the stars!',
-           author: 'Ringo Starry',
-           date: '2013-12-02T17:57:28.556094Z'
-       },
-       {
-           rating: 2,
-           comment: 'It\'s your birthday, we\'re gonna party!',
-           author: '25 Cent',
-           date: '2011-12-02T17:57:28.556094Z'
-       }
-   ]
-};
 
 @Component({
   selector: 'app-dishdetail',
@@ -58,19 +17,89 @@ const DISH = {
 })
 export class DishdetailComponent implements OnInit {
 
+  @ViewChild('cform') commentFormDirective;
+
   dish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
+  commentForm: FormGroup;
+  comment: Comment;
+
+
+  formErrors = {
+    'comment': '',
+    'author': ''
+  };
+
+  validationMessages = {
+    'comment': {
+      'required':      'Fill in Your Comment.',
+    },
+    'author': {
+      'required':      'Author Name is required.',
+      'minlength':     'Author Name must be at least 2 characters long.',
+      'maxlength':     'Author Name cannot be more than 35 characters long.'
+    },
+  };
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder ) { this.createForm(); }
 
     ngOnInit() {
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
       this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+    }
+
+    createForm() {
+      this.commentForm = this.fb.group({
+        comment: ['', [Validators.required] ],
+        rating: 5,
+        author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(35)] ],
+
+      });
+
+      this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  
+    this.onValueChanged(); // (re)set validation messages now
+    }
+
+    onValueChanged(data?: any) {
+      if (!this.commentForm) { return; }
+      const form = this.commentForm;
+      for (const field in this.formErrors) {
+        if (this.formErrors.hasOwnProperty(field)) {
+          // clear previous error message (if any)
+          this.formErrors[field] = '';
+          const control = form.get(field);
+          if (control && control.dirty && !control.valid) {
+            const messages = this.validationMessages[field];
+            for (const key in control.errors) {
+              if (control.errors.hasOwnProperty(key)) {
+                this.formErrors[field] += messages[key] + ' ';
+              }
+            }
+          }
+        }
+      }
+    }
+
+    onSubmit() {
+      let date = new Date().toISOString();
+      this.comment = this.commentForm.value;
+      this.comment.date = date;
+      console.log(this.comment);
+      this.dish.comments.push(this.comment);
+      this.commentForm.reset({
+        comment: '',
+        rating: 5,
+        author: ''
+      });
+      this.commentFormDirective.resetForm(this.commentForm.value);
     }
   
     setPrevNext(dishId: string) {
